4a46c70180b86c1745123d4cfb77d0ffd4374943
JC <julio.cesar.salvino12@gmail.com>
2024-12-03 14:07:10 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,65 @@
+#include #include #include
+#define MAX 101
+
+struct digitos
+{
+    char num1;
+    char num2;
+    char operation;
+};
+
+char num_armnz(char n[MAX])
+{
+    for (int i = 0; i < 0; i++)
+    {
+        if (int n >= 10)
+        {
+            subiu = 1;
+        }
+        else
+        {
+            subiu = 0;
+        }
+    }
+}
+
+char ops(char n[20])
+{
+    if (char n[20] = 1)
+    {
+        return 1;
+    }
+    else if
+    {
+        (char n[20] = 2) return 2;
+    }
+    else if
+    {
+        (char n[20] = 3) return 3;
+    }
+    else if
+    {
+        (char n[20] = 4) return 4;
+    }
+    else
+    {
+        exit(0);
+    }
+}
+struct digitos;
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    printf("Forneça um número: ");
+    scanf("%c", digitos.num1);
+
+    printf("Forneça outro número: ");
+    scanf("%c", digitos.num2);
+
+    printf("Escolha sua operação(Soma(1), Subtração(2), Multiplicação(3), Divisão(4):");
+    scanf("%c", digitos.operation);
+    digitos.operation = ops;
+    return 0;
+}
